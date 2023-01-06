6cb7d701700babe7eaa3d45d8a5811adb7b68cfb
ctang415 <carrietang117@gmail.com>
2023-01-06 14:28:21 -0500

    Created ship factory function with tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+const Ship = (length) => {
+    let shipSunk = false;
+    const array = [];
+    for (let i = 0; i < length; i++) {
+        array.push({xy: null, hit:false})
+    }
+    const isHit = (coordinate) => {
+        let shipPart = array.find(element => element.xy === coordinate)
+        return shipPart.hit = true;
+    }
+    const isSunk = () => {
+        const isTrue = (element) => element.hit === true
+        if (array.every(isTrue)) {
+        return shipSunk = true;
+        }
+        return shipSunk;
+    }
+    return { array, isHit, isSunk }
+}
+
+module.exports = Ship
